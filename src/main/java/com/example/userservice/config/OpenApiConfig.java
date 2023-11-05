69462e009dafafdee7e789ba7880c3e46dc0147d
bravodivs <vermadevanshu17@gmailcom>
2023-11-05 21:07:59 +0530

    Swagger configured for authorization bearer token

--- src/main/java/com/example/userservice/config/OpenApiConfig.java
+++ src/main/java/com/example/userservice/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.example.userservice.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +13,22 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI apiConfig() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("User service micro application")
                         .description("User service application containing user apis")
-                        .version("1.0"));
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components().addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT"))
+                );
     }
 
 }
